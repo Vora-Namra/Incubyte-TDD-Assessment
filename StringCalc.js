263691371b3b9b5aaf058758a3c5ae870e90c539
unknown <voranamra625@gmail.com>
2025-07-03 18:14:05 +0530

    passed the empty string which returns 0 as sum

--- StringCalc.js
+++ StringCalc.js
@@ -1,7 +1,14 @@
-function add(str){
-    
-        const num = parseInt(str);
-            return num;
+function add(str) {
+    if (str.trim() === "") return 0;
+
+    const nums = str.split(",").map(num => parseInt(num));
+
+    if (nums.length === 1) {
+        return nums[0];
     }
+
+    return 0;
+
+}
 
 module.exports = add;
--- test/StringCalc.test.js
+++ test/StringCalc.test.js
@@ -5,10 +5,13 @@ describe('StringCalc', () => {
     it('should return 0 for empty string', () => {
         expect(add("")).toBe(0);
     });
-    
+
     it('should return integer of given string',()=>{
         expect(add("2")).toBe(2);
     })
+    it('should return sum for two values', () => {
+        expect(add("2,3")).toBe(5);
+    });
 
 
 });
